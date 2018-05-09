2085448b5adc08e20fe40ebcd35102c7725a3fda
fouadpy <perceval@protonmail.com>
2018-05-09 22:33:54 +0200

    adding About pages and route's pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    # if current_user
+    #   redirect_to tasks_path
+    # end
   end
+
+  def about
+  end
+
 end
