4a850fc2f7119ab0cda53b2110a1f2b93456df97
fouadpy <perceval@protonmail.com>
2019-02-17 20:06:02 +0100

    Made test page with task panel

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,15 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    # if current_user
-    #   redirect_to tasks_path
-    # end
+    if current_user
+      redirect_to tasks_path
+    end
   end
 
   def about
   end
 
+  def test
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
   resources :tasks do
     member do
       patch :change
     end
   end
+
+  get 'about' => 'pages#about'
+  get 'test'  => 'pages#test'
 end
